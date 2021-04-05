e2b9358f79f88f4b6a23a979f2ee78c6675ae537
blanketmanatee <ashleyl.welch@gmail.com>
2021-04-05 03:23:52 +0000

    advanced

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# uses puppet to set up config file
+
+include stdlib
+file_line {'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '	IdentityFile ~/.ssh/holberton',
+  replace => true,
+}
+
+file_line {'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '	PasswordAuthentication no',
+  replace => true,
+}
