d080e5dbf63042a6b97b850ce3aca855108d0809
blanketmanatee <ashleyl.welch@gmail.com>
2021-04-15 01:49:48 +0000

    advanced done

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# use puppet to install nginix
+exec {'Install nginx':
+command  => 'sudo apt update && sudo apt -y install nginx && echo "Holberton School" | sudo tee /var/www/html/index.html && redirect="\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\n" && sudo sed -i "37i $redirect" /etc/nginx/sites-enabled/default && sudo service nginx restart',
+provider => shell,
+}
