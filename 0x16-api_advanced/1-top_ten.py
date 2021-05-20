07fd3162cd504424e473034e74ce47d5a587ef70
blanketmanatee <ashleyl.welch@gmail.com>
2021-05-20 04:59:46 +0000

    pep again

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,9 +6,9 @@ import requests
 
 def top_ten(subreddit):
     """ titles of first 10 hot posts in a sub """
-    url="https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    user_agent={"User-Agent": "bobthebuilder"}
-    request=requests.get(url, headers=user_agent)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    user_agent = {"User-Agent": "bobthebuilder"}
+    request = requests.get(url, headers=user_agent)
     if request.status_code == 200:
         request = request.json()
         data = request.get("data")
